995e32732d741d288a48a08edf004400f241b415
AlexandreWeber <alexandre.dalabona@gmail.com>
2020-02-18 11:07:17 -0300

    Comentários do data binding e diretivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,20 +10,43 @@ import { PoMenuItem } from '@portinari/portinari-ui';
 export class AppComponent {
   name = 'TOTVS';
   inputValue = 'Teste';
+  type = 2;
+  nameTeste = 'Alexandre';
+  showName2 = false;
+
+  // Essa variável é usado no HTML
+  readonly menus: Array<PoMenuItem> = [
+    { label: 'Home', action: this.onClick.bind(this) },
+    { label: 'Clientes', action: this.onClick.bind(this) }
+  ];
+
+  isShowRed = true;
+
+  showName = false;
+  // Esse array é utilizado no *ngFor
+  customerList = [
+    {
+      name: 'TOTVS',
+      orders: [{ code: 1, value: 10 }, { code: 2, value: 20 }]
+    },
+    {
+      name: 'Netflix',
+      orders: [{ code: 100, value: 1000 }, { code: 200, value: 2000 }]
+    }
+  ];
 
   literals = {
-    buttonLabel: 'Clique Aqui!!!',
+    buttonLabel: 'Clique Aqui!!!', // Essa variavel é usada o HTML para mostrar a label do botão
     inputLabel: 'Two-Way'
   }
 
-  readonly menus: Array<PoMenuItem> = [
-    { label: 'Home', action: this.onClick.bind(this) }
-  ];
+
 
   private onClick() {
     alert('Clicked in menu item')
   }
 
+  // Essa função está no botão do HTML
   changeName() {
     this.name = `${this.name} Alterado`;
   }
@@ -32,4 +55,8 @@ export class AppComponent {
     this.inputValue = 'Alterado no TS';
   }
 
+
+  canShowName() {
+    return true;
+  }
 }
